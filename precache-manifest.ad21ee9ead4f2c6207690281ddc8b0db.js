1a14cd5f56c1c9677497a36c7880e4538212ce14
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-10-17 04:22:48 +0100

    Updates

---
+++ precache-manifest.ad21ee9ead4f2c6207690281ddc8b0db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235d4051fdb694f7267ab518f9c96962",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "185d601fdff22883d70d",
+    "url": "/monsters-rolodex/static/css/main.974f8e79.chunk.css"
+  },
+  {
+    "revision": "f977fa80ff73af86627e",
+    "url": "/monsters-rolodex/static/js/2.37e904de.chunk.js"
+  },
+  {
+    "revision": "185d601fdff22883d70d",
+    "url": "/monsters-rolodex/static/js/main.95a6a648.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
